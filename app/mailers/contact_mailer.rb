9d1aeeb915faec08ea0f5099499c50da40b144b5
Ricardo Mejia <creationrebelus@gmail.com>
2017-09-18 19:47:17 +0000

    bug fix contact controller

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default to: 'ramejia76@gmail.com'
   
-  def create_email(name, email, body)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
