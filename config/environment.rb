45a37c4bc2e5d39c9a50c00383ff6f6e3ad01833
Ricardo Mejia <creationrebelus@gmail.com>
2017-09-19 17:04:59 +0000

    added mailgun mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port => ENV['MAILGUN_SMTP_PORT'],
-  :address => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-  :password => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain => 'safe-taiga-14773.herokuapp.com'
-  :authentication => :plain,
+  :port             => ENV['MAILGUN_SMTP_PORT'],
+  :address          => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name        => ENV['MAILGUN_SMTP_LOGIN'],
+  :password         => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain           => 'safe-taiga-14773.herokuapp.com',
+  :authentication   => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
